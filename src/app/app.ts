35107d7563a0893760484cf8269680e99e6bfb20
IronInkai <i202315741@cibertec.edu.pe>
2026-04-18 07:54:12 -0500

    Creacion del dashboard del admin

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './components/login/login'; 
+//import { Login } from './components/login/login'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Login], 
+  imports: [RouterOutlet, /*Login*/], 
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
